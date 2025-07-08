870da22e4b93f94fc213b64adfacca87914960ca
Ranjith.Kumar <theranjith@gmail.com>
2025-07-08 22:42:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import "./App.css";
+import { InfinitePeople } from "./people/InfinitePeople";
+import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        <ReactQueryDevtools />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
